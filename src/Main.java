7d81b1b5222362efeb35bcb77bf7d8468ed884bc
Young Jun <yj112@duke.edu>
2022-02-14 16:20:00 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main extends Application to use frontend featured from javaFx
+ */
+public class Main extends Application {
+
+
+    /**
+     * The start methods serves the role of a main method of this project.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+    }
+}
