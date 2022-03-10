c1a0b21ffdce420530df2d47313dcf511d6188cd
Young Jun <yj112@duke.edu>
2022-03-10 15:39:55 +0900

    added class ReflectionHandler, created by Ryleigh Byrne, while refactoring class ChessEngine #4

---
+++ src/java/model/util/ReflectionHandler.java
@@ -0,0 +1,33 @@
+package model.util;
+
+import java.lang.reflect.Method;
+
+/**
+ * Class to handle reflection
+ *
+ * @author Ryleigh Byrne, Young Jun
+ */
+public class ReflectionHandler {
+
+    /**
+     * Handle method given a method name and a class name
+     * @param name of method to call
+     * @param aClass name of class to call the method in
+     * @return desired method
+     */
+    public Method handleMethod(String name, String aClass) {
+        try {
+            Class<?> thisClass = Class.forName(aClass);
+            Method m = thisClass.getDeclaredMethod(name);
+            return m;
+        } catch (NoSuchMethodException e) {
+            String error = String.format("The method: %s could not be generated. Double check method you are trying to call's name", name);
+            System.out.println(error);
+            return null;
+        }catch(ClassNotFoundException e){
+            String error = String.format("The class: %s could not be generated. Double check class you are trying to call's name", name);
+            System.out.println(error);
+            return null;
+        }
+    }
+}
