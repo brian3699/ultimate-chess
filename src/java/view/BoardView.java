c517b282951a4036db1805486221d850318beac9
Young Jun <yj112@duke.edu>
2022-03-14 23:52:32 +0900

    Changed class PieceView to TileView and made it extend javafx.Region. Also started writing class BoardView #8

---
+++ src/java/view/BoardView.java
@@ -0,0 +1,83 @@
+package view;
+
+import javafx.scene.layout.GridPane;
+
+import java.awt.*;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+public class BoardView {
+
+    private static final String MAGIC_VALUE_RESOURCE_PATH = "view.resources.MagicValues";
+
+    private final GridPane myBoard;
+    private final int rowCount;
+    private final int colCount;
+    private final Consumer clickMethod;
+    private final ResourceBundle magicValueBundle;
+    private final TileView[][] boardArray;
+    private List<Point> possibleMoves;
+
+    public BoardView(Consumer<Point> clickMethod, int rowCount, int colCount) {
+        this.clickMethod = clickMethod;
+        this.rowCount = rowCount;
+        this.colCount = colCount;
+        boardArray = new TileView[rowCount][colCount];
+        myBoard = new GridPane();
+        magicValueBundle = ResourceBundle.getBundle(MAGIC_VALUE_RESOURCE_PATH);
+    }
+
+    public void setTile(int rowNum, int colNum, String pieceType){
+        TileView tile = new TileView(pieceType);
+        addTileToBoard(tile, rowNum, colNum);
+    }
+
+    public void movePiece(int xOrigin, int yOrigin, int xNew, int yNew){
+        removeHighlight();
+        TileView piece = boardArray[yOrigin][xOrigin];
+        TileView destinationTile = boardArray[yNew][xNew];
+        addTileToBoard(piece, yNew, xNew);
+        addTileToBoard(destinationTile, yOrigin, xOrigin);
+    }
+
+    public void capturePiece(int xOrigin, int yOrigin, int xNew, int yNew){
+        removeHighlight();
+        TileView piece = boardArray[yOrigin][xOrigin];
+        TileView emptyTile = new TileView("-");
+        addTileToBoard(piece, yNew, xNew);
+        addTileToBoard(emptyTile, yOrigin, xOrigin);
+    }
+
+    /**
+     * Highlight possible moves in green.
+     *
+     * @param possibleMoves List of points with possible moves.
+     */
+    public void highlightPossibleMoves(List<Point> possibleMoves) {
+        this.possibleMoves = possibleMoves;
+        for (Point move : possibleMoves) {
+            TileView tile = boardArray[move.y][move.x];
+            tile.setBackground(3);
+            myBoard.add(tile, move.y, move.x);
+        }
+    }
+
+    private void removeHighlight(){
+        for (Point move : possibleMoves) {
+            TileView tile = boardArray[move.y][move.x];
+            addTileToBoard(tile, move.y, move.x);
+        }
+    }
+
+    private void setTileColor(TileView tile, int rowNum, int colNum){
+        int tileColor = ((rowNum % 2) + colNum)%2;
+        tile.setBackground(tileColor);
+    }
+
+    private void addTileToBoard(TileView tile, int rowNum, int colNum){
+        setTileColor(tile, rowNum, colNum);
+        boardArray[rowNum][colNum] = tile;
+        myBoard.add(tile, colNum, rowNum);
+    }
+}
--- src/java/view/TileView.java
+++ src/java/view/TileView.java
@@ -1,42 +1,43 @@
 package view;
 
-import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
 import java.util.ResourceBundle;
 
-public class PieceView implements PieceViewInterface{
+public class TileView extends Region {
     private static final String MAGIC_VALUE_RESOURCE_PATH = "view.resources.MagicValues";
     private static final String TILE_SIZE = "tileSize";
     private final ResourceBundle magicValueResource;
     private final HBox myPiece;
+    private final Image pieceImage;
 
-    public PieceView(Image pieceImage) {
+    public TileView(String pieceType) {
         // Initialize a resource bundle containing all magic values
         magicValueResource = ResourceBundle.getBundle(MAGIC_VALUE_RESOURCE_PATH);
         // HBox is used instead of a rectangle to set background
         myPiece = new HBox();
-        setPieceNode(pieceImage);
+        pieceImage = new Image(magicValueResource.getString(pieceType + "_Image"));
+        setTile(pieceImage);
     }
 
     // Initialize a piece node and insert an image.
-    private void setPieceNode(Image pieceImage){
+    private void setTile(Image pieceImage){
         int tileSize = Integer.parseInt(magicValueResource.getString(TILE_SIZE));
         Rectangle rectangle = new Rectangle(tileSize, tileSize);
         // fill the rectangle with image
         rectangle.setFill(new ImagePattern(pieceImage));
-        // add rectangle to HBox myPiece
-        myPiece.getChildren().add(rectangle);
+        // add rectangle
+        getChildren().add(rectangle);
     }
 
     public void setBackground(int colorID){
         // get color from magicValueResource
-        String color = magicValueResource.getString(String.valueOf(colorID));
-        myPiece.setStyle(color);
+        String colorHex = magicValueResource.getString(String.valueOf(colorID));
+        setStyle(colorHex);
     }
 
-    public Node getPieceNode() {return myPiece;}
 }
